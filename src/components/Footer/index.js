7339c8abcb61929326e63b222e7299845066d1ed
Emerson P. Gomes <emersonpgomes@gmail.com>
2021-01-27 22:26:38 -0300

    instalação e correções do eslint

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
-import styled from 'styled-components'
-
+import React from 'react';
+import styled from 'styled-components';
 // src/components/Footer/index.js
+
 const FooterWrapper = styled.footer`
   background-color: #00000070;
   padding: 20px;
